4edd8754e0766de146ef512dbc0108b41af5b0df
sblyu <h0939605719@gmail.com>
2024-03-18 10:14:46 +0800

    springboot-member 檢查註冊的email、隱藏返回的密碼

--- src/main/java/com/sblyu/springbootmember/dao/UserDao.java
+++ src/main/java/com/sblyu/springbootmember/dao/UserDao.java
@@ -6,5 +6,7 @@ import com.sblyu.springbootmember.model.User;
 public interface UserDao {
 
     User getUserById(Integer userId);
+
+    User getUserByEmail(String email);
     Integer createUser(UserRegisterRequest userRegisterRequest);
 }
